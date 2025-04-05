ef16fc3c0390795500e5e8d47c386080a7ad490c
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2025-04-06 02:11:49 +0330

    feat: Addded OrderPlacementController & Request clasess (#WIP)

---
+++ app/Http/Controllers/V1/OrderPlacementController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\OrderPlacementRequest;
+
+class OrderPlacementController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(OrderPlacementRequest $request)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\AuthController;
+use App\Http\Controllers\V1\OrderPlacementController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function() {
@@ -8,4 +9,6 @@ Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function() {
     Route::get('/whoami', [AuthController::class, 'whoami'])->name('user.whoami');
 
     Route::get('/coins', [\App\Http\Controllers\V1\CoinController::class, 'index'])->withoutMiddleware('auth:sanctum')->name('coins.index');
+
+    Route::post('/orders', OrderPlacementController::class)->name('orders.placement');
 });
